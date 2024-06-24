1abcdaeabdcf9c1bab499e6c164fbf25d1cc05de
hieupham250 <ptrunghieu2507@gmail.com>
2024-06-24 10:28:47 +0700

    bai tap

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import B1 from "./components/B1";
+import B2 from "./components/B2";
+import B3 from "./components/B3";
+import B4 from "./components/B4";
+import B6 from "./components/B6";
+
+export default function App() {
+  return (
+    <div>
+      <B1></B1>
+      <B2></B2>
+      <B3></B3>
+      <B4></B4>
+      <B6></B6>
+    </div>
+  );
+}
